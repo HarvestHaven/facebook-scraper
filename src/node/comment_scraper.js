ce641fed3863e3e690343a489107b7702202f0f6
mikepreston17 <michael.n.preston@gmail.com>
2023-02-03 07:53:59 -0600

    ++ My attempts at getting ScrepingBee to grab comments for us

---
+++ src/node/comment_scraper.js
@@ -0,0 +1,49 @@
+// request Axios
+const axios = require("axios");
+const fs = require("fs");
+const scrapingbee = require('scrapingbee');
+const more_replies_xpath =
+  "//div[@id='contentArea']//a[div/span[contains(text(),'View') and (contains(text(),'more replies') or contains(text(),'more comment'))]]";
+
+async function get(url) {
+  var client = new scrapingbee.ScrapingBeeClient('QFPOISA2OZDYCPLXWPJPL0UP4ZE9B999D0F7X7J80X3LGBU2CKSW80HK9KNEI24FA451D46I1QANAV4J');
+  var response = await client.get({
+    url: url,
+    params: {  
+    },
+  })
+  return response
+}
+
+get('https://app.scrapingbee.com/api/v1').then(function (response) {
+    var decoder = new TextDecoder();
+    var text = decoder.decode(response.data);
+    console.log(text);
+}).catch((e) => console.log('A problem occurs : ' + e.response.data));
+
+
+// axios
+//   .get("https://app.scrapingbee.com/api/v1", {
+//     params: {
+//       api_key:
+//         "QFPOISA2OZDYCPLXWPJPL0UP4ZE9B999D0F7X7J80X3LGBU2CKSW80HK9KNEI24FA451D46I1QANAV4J",
+//       url: "https://www.facebook.com/stephanie.reinke.92/posts/pfbid02Q9rYwrWFvme7e7VRXU1waKewW8VzUDAPCgi6k7q83yFzDsYvYYDpNKfHnn2EBHXl",
+
+
+//     //   'js_scenario': {"instructions": [{"click": "form > div > div > button"}, {'wait': '1500' }]} 
+
+//     },
+
+//     // instructions: [
+//     //   { wait_for_and_click: "#contentArea" },
+//     //   { wait: 250 },
+//     // ],
+//   })
+//   .then(function (response) {
+//     // handle success
+//     console.log(response);
+//     let raw_html = JSON.stringify(response.data).replace('\\\\"', '\\"');
+
+//     fs.writeFileSync("response.html", raw_html, {});
+//   })
+//   .catch(console.error);
