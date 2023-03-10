2ad412c2dace0321eff89940ef5a86222f49d24d
Igor <7148363+igisp@users.noreply.github.com>
2023-03-10 09:40:15 -0700

    Updated version of Chromedriver and Chromium to 111; pinned version of Selenium to 4.2.0

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 docker run -v $(pwd)/volumes:/var/volumes -v $(pwd)/post_list.json:/app/post_list.json fb_posts /var/volumes /app/post_list.json                                                                  
 docker build -t fb_posts .
+
+sudo docker run -v ~/test/volumes:/var/volumes --entrypoint python fb_posts scraper.py /var/volumes <fb_post_url>
+# ex: sudo docker run -v ~/test/volumes:/var/volumes --entrypoint python fb_posts scraper.py /var/volumes "https://www.facebook.com/iamthatprophet/videos/2530938387225313"
